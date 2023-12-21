fa13ec82a1e85545401dc5c96bc6c614d5ce52b0
Doucoure kanthio <kanthiodoucoure5@gmail.com>
2023-12-21 16:32:46 +0100

    Message de validation pour les modifications des fichiers HTML et CSS

--- src/pages/Legale.js
+++ src/pages/Legale.js
@@ -1,99 +1,58 @@
-import React from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import '../App.css'; 
 
 const Legale = () => {
+  const [showEditor, setShowEditor] = useState(false);
+  const [showHost, setShowHost] = useState(false);
+  const [showCredits, setShowCredits] = useState(false);
+
   return (
     <div className="container mt-5">
       <h1>Mentions Légales</h1>
-      <div className="accordion" id="legalAccordion">
-        {/* Section 1 - Éditeur du site */}
-        <div className="card">
-          <div className="card-header" id="editorHeading">
-            <h2 className="mb-0">
-              <button
-                className="btn btn-link"
-                type="button"
-                data-toggle="collapse"
-                data-target="#editorCollapse"
-                aria-expanded="true"
-                aria-controls="editorCollapse"
-              >
-                Éditeur du site
-              </button>
-            </h2>
-          </div>
-          <div
-            id="editorCollapse"
-            className="collapse"
-            aria-labelledby="editorHeading"
-            data-parent="#legalAccordion"
-          >
-            <div className="card-body">
-              <p>Nom de l'éditeur du site : [Nom de l'éditeur]</p>
-              <p>Adresse : [Adresse]</p>
-              <p>Contact : [Adresse email ou numéro de téléphone]</p>
-              <p>Registre du commerce et des sociétés (RCS) : [Numéro RCS]</p>
-            </div>
-          </div>
-        </div>
 
-        {/* Section 2 - Hébergeur du site */}
-        <div className="card">
-          <div className="card-header" id="hostHeading">
-            <h2 className="mb-0">
-              <button
-                className="btn btn-link"
-                type="button"
-                data-toggle="collapse"
-                data-target="#hostCollapse"
-                aria-expanded="false"
-                aria-controls="hostCollapse"
-              >
-                Hébergeur du site
-              </button>
-            </h2>
+      {/* Section 1 - Éditeur du site */}
+      <div>
+        <h2 onClick={() => setShowEditor(!showEditor)}>Éditeur du site</h2>
+        {showEditor && (
+          <div>
+            <p>Nom de l'éditeur du site : cv-john-doe</p>
+            <p>Adresse : 1 Rue de la Légion d'Honneur</p>
+            <p>Contact : 0606060606</p>
+        
           </div>
-          <div
-            id="hostCollapse"
-            className="collapse"
-            aria-labelledby="hostHeading"
-            data-parent="#legalAccordion"
-          >
-            <div className="card-body">
-              <p>Nom de l'hébergeur du site : [Nom de l'hébergeur]</p>
-              <p>Adresse : [Adresse]</p>
-              <p>Contact : [Adresse email ou numéro de téléphone]</p>
-            </div>
-          </div>
-        </div>
+        )}
+      </div>
 
-        {/* Section 3 - Crédits des images */}
-        <div className="card">
-          <div className="card-header" id="creditsHeading">
-            <h2 className="mb-0">
-              <button
-                className="btn btn-link"
-                type="button"
-                data-toggle="collapse"
-                data-target="#creditsCollapse"
-                aria-expanded="false"
-                aria-controls="creditsCollapse"
+      {/* Section 2 - Hébergeur du site */}
+      <div>
+        <h2 onClick={() => setShowHost(!showHost)}>Hébergeur du site</h2>
+        {showHost && (
+          <div>
+            <p>Nom de l'hébergeur du site : Github-Pages</p>
+            <p>Adresse : <a href="https://github.com/" target="_blank" rel="noopener noreferrer">https://github.com/</a></p>
+            <p>Contact : Via la plateforme GitHub</p>
+          </div>
+        )}
+      </div>
+
+      {/* Section 3 - Crédits des images */}
+      <div>
+        <h2 onClick={() => setShowCredits(!showCredits)}>Crédits des images</h2>
+        {showCredits && (
+          <div>
+            <p>
+              Les images utilisées sur ce site proviennent de{' '}
+              <a
+                href="https://pixabay.com/"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                Crédits des images
-              </button>
-            </h2>
+                Pixabay
+              </a>
+              .
+            </p>
           </div>
-          <div
-            id="creditsCollapse"
-            className="collapse"
-            aria-labelledby="creditsHeading"
-            data-parent="#legalAccordion"
-          >
-            <div className="card-body">
-              <p>Les images utilisées sur ce site proviennent de <a href="https://pixabay.com/" target="_blank" rel="noopener noreferrer">Pixabay</a>.</p>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
